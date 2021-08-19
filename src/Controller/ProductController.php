4eccf4f4b7aca9b5bfb1d2e001b8d7cab50ebb38
Alexis Juglair <alexis.juglair@gmail.com>
2021-08-19 16:54:33 +0200

    Création de formulaires

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use PhpParser\Node\Stmt\Return_;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductController extends AbstractController
 {
@@ -48,6 +54,43 @@ class ProductController extends AbstractController
 
         return $this->render('product/show.html.twig', [
             'product' => $product
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/create", name="product_create")
+     */
+    public function create(FormFactoryInterface $factory) 
+    {
+        $builder = $factory->createBuilder();
+
+        $builder->add("name", TextType::class, [
+            'label' => 'Nom du produit',
+            'attr' => ['placeholder' => 'Tapez le nom du produit']
+        ])
+            ->add('shortDescription', TextareaType::class, [
+                'label' => 'Description courte',
+                'attr' => ['placeholder' => 'Tapez une description assez courte mais parlante pour le visiteur']
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix du produit',
+                'attr' => ['placeholder' => 'Tapez le prix du produit en €']
+            ])
+            ->add('category', EntityType::class, [
+                'label' => 'Catégorie',
+                'placeholder' => '-- Choisir une catégorie --',
+                'class' => Category::class,
+                'choice_label' => function(Category $category) {
+                    return strtoupper($category->getName());
+                }
+            ]);
+
+        $form = $builder->getForm();
+
+        $formView = $form->createView();
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView
         ]);
     }
 }
