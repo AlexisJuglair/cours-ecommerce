6ae0876e909eeb51553f934c8e2c6abb2b3682dc
AlexisJuglair <53174226+AlexisJuglair@users.noreply.github.com>
2021-08-21 17:58:16 +0200

    Découverte de fonctionalités avancées du composants Form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,7 +11,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
@@ -70,7 +69,8 @@ class ProductController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted()) 
-        {
+        {    
+            dd($form->getData());
             $em->flush();
 
             return $this->redirectToRoute('product_show', [
