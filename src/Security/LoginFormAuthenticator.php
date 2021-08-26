17c6fcfde3e28c14e66c3c91c5f4c7d3c14194d0
Alexis Juglair <alexis.juglair@gmail.com>
2021-08-26 13:35:55 +0200

    LoginFormAuthentificator

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class LoginFormAuthenticator extends AbstractGuardAuthenticator
+{
+    protected $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function supports(Request $request)
+    {
+        return $request->attributes->get('_route') === 'security_login'
+            && $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        return $request->get('login'); //array avec 3 infos
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        return $userProvider->loadUserByUsername($credentials["email"]);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // Vérifier sue le mot de passe fourni, coorespond bien au mot de passe de la base de donnée
+        // $credentials['password'] => $user->getPassword()
+        return $this->encoder->isPasswordValid($user, $credentials['password']);
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        // todo
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
+    {
+        dd("Success");
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        // todo
+    }
+
+    public function supportsRememberMe()
+    {
+        // todo
+    }
+}
