8ba16aa347e0cbb2e034589d6e69836e7a36cb83
Alexis Juglair <alexis.juglair@gmail.com>
2021-08-26 15:02:34 +0200

    authentification2

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,21 +4,23 @@ namespace App\Controller;
 
 use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="security_login")
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $utils, FormFactoryInterface $factory): Response
     {
-
-        $form = $this->createForm(LoginType::class);
+        $form = $this->createForm(LoginType::class, ['email' => $utils->getLastUsername()]);
 
         return $this->render('security/login.html.twig', [
-            'formView' => $form->createView()
+            'formView' => $form->createView(),
+            'error' => $utils->getLastAuthenticationError()
         ]);
     }
 }
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,10 +2,13 @@
 
 namespace App\Security;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
@@ -32,24 +35,42 @@ class LoginFormAuthenticator extends AbstractGuardAuthenticator
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        return $userProvider->loadUserByUsername($credentials["email"]);
+        try 
+        {
+            return $userProvider->loadUserByUsername($credentials["email"]);
+        }
+        catch (UsernameNotFoundException $e)
+        {
+            throw new AuthenticationException("Cette adresse email n'est pas connu");
+        }
+        
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
         // Vérifier sue le mot de passe fourni, coorespond bien au mot de passe de la base de donnée
         // $credentials['password'] => $user->getPassword()
-        return $this->encoder->isPasswordValid($user, $credentials['password']);
+
+        $isValid = $this->encoder->isPasswordValid($user, $credentials['password']);
+
+        if (!$isValid)
+        {
+            throw new AuthenticationException("Les informations de connexion ne correspondent pas");
+        }
+
+        return true;      
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
-        // todo
+        $login = $request->request->get('login');
+        $request->attributes->set(Security::LAST_USERNAME, $login['email']);
+        $request->attributes->set(Security::AUTHENTICATION_ERROR, $exception);
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
-        dd("Success");
+        return new RedirectResponse('/');
     }
 
     public function start(Request $request, AuthenticationException $authException = null)
