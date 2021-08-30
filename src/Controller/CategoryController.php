a1310526575603dd4ba9dbf9b8158453438ebf9b
Alexis Juglair <alexis.juglair@gmail.com>
2021-08-30 16:43:54 +0200

    Introduction à la sécurité dans notre application

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -9,8 +9,12 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CategoryController extends AbstractController
 {
@@ -62,9 +66,13 @@ class CategoryController extends AbstractController
     /**
      * @Route("/admin/category/{id}/edit", name="category_edit")
      */
-    public function edit($id, CategoryRepository $categoryRepository, Request $request, EntityManagerInterface $em): Response
+    public function edit($id, CategoryRepository $categoryRepository, Request $request, EntityManagerInterface $em, Security $security): Response
     {
         $category = $categoryRepository->find($id);
+
+        if(!$category){
+            throw new NotFoundHttpException("Cette catégorie n'existe pas");
+        }
 
         $form = $this->createForm(CategoryType::class, $category);
 
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SecurityController extends AbstractController
 {
@@ -22,5 +23,13 @@ class SecurityController extends AbstractController
             'formView' => $form->createView(),
             'error' => $utils->getLastAuthenticationError()
         ]);
+    }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout()
+    {
+       
     }
 }
