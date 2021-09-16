9460ba2d91f59d3255881372378a8034e9c901cb
Alexis Juglair <alexis.juglair@gmail.com>
2021-09-16 13:39:32 +0200

    commandes

--- src/Controller/Purchase/PurchasesListController.php
+++ src/Controller/Purchase/PurchasesListController.php
@@ -3,16 +3,48 @@
 namespace App\Controller\Purchase;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Security;
+use Twig\Environment;
 
-class PuchasesListController extends AbstractController
+class PurchasesListController extends AbstractController
 {
+    protected $security;
+    protected $router;
+    protected $twig;
+
+    public function __construct(Security $security, RouterInterface $router, Environment $twig)
+    {
+        $this->security = $security;
+        $this->router = $router;
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/purchases", name="purchase_index")
+     */
     public function index()
     {
-        // 1. Nous devons nous assurer que la personne est connectée(sinon redirection vers la page d'accueil)
+        // 1. Nous devons nous assurer que la personne est connectée (sinon redirection vers la page d'accueil) -> Security
+        /** @var User */
+        $user = $this->security->getUser();
 
-        // 2. Nous voulons savoir QUI est connecté
+        if (!$user) 
+        {
+            // Générer une URL en fonction du nom d'une route -> UrlGeneratorRouterInterface
+            throw new AccessDeniedException("Vous devez être connecté pour accéder à vos commandes");
+            
+        }
 
-        // 3. Nous voulons passer l'utilsiateur connecté à Twig afin d'afficher ses commandes
+        // 2. Nous voulons savoir QUI est connecté -> Security
+        // 3. Nous voulons passer l'utilsiateur connecté à Twig afin d'afficher ses commandes -> Environment de twig / Response
+        $html = $this->twig->render('purchase/purchase.html.twig', [
+            'purchases' => $user->getPurchases()
+        ]);
+        return new Response($html);
     }
 }
-
